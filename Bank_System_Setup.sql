4326eaf077813017dd170d49bb859760b2cd353e
SerhanToka <serhantokaa@hotmail.com>
2025-12-02 21:16:47 +0300

    feat: Create 'Customers' table

--- Bank_System_Setup.sql
+++ Bank_System_Setup.sql
@@ -3,3 +3,10 @@
 
 -- Creating Database
 CREATE DATABASE Bank_System;
+
+CREATE TABLE Customers (
+    customer_id INT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+);
