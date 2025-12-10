d151458bc46e4bfa67a1bea886a537f09fd92a42
SerhanToka <serhantokaa@hotmail.com>
2025-12-10 20:06:39 +0300

    feat: Add sample inputs for Transactions table, fix: Typo fix

--- Bank_System_Setup.sql
+++ Bank_System_Setup.sql
@@ -25,7 +25,7 @@ CREATE TABLE Transactions (
     transaction_id INT PRIMARY KEY AUTO_INCREMENT,
     account_id INT,
     amount DECIMAL(10, 2) NOT NULL,
-    transaction_type VARCHAR(10) DEFAULT "Personal",
+    transaction_type VARCHAR(10),
     transaction_date DATETIME DEFAULT NOW(),
     FOREIGN KEY (account_id) REFERENCES Accounts(account_id),
     CHECK (amount > 0) -- Basic Fraud Security: No Negative Transaction
@@ -40,8 +40,19 @@ VALUES  ("Shane","Job","shanejob@mail.com"),
         ("Courtney","Lord","courtneylord@mail.com");
 
 INSERT INTO Accounts (customer_id, balance)
-VALUES  (1, 8294.13)
-        (2, 128216.50)
-        (3, 25045.45)
-        (4, 0.00)
-        (5, 1223414.25)
+VALUES  (1, 8294.13),
+        (2, 128216.50),
+        (3, 25045.45),
+        (4, 0.00),
+        (5, 1223414.25);
+
+-- Fraud Scenario
+-- Test transaction for account activity
+INSERT INTO Transactions (account_id, amount, transaction_type, transaction_date)
+VALUES (4, 1.00, 'Deposit', '2023-10-26 14:00:00');
+-- Placement of stolen money
+INSERT INTO Transactions (account_id, amount, transaction_type, transaction_date)
+VALUES (4, 48500.00, 'Deposit', '2023-10-26 14:05:00');
+-- Layering
+INSERT INTO Transactions (account_id, amount, transaction_type, transaction_date)
+VALUES (4, 48000.00, 'Withdraw', '2023-10-26 14:15:00');
